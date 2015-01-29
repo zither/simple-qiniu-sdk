b26d1e2553058df0cfeb234f12192ad42e1a540b
JonChou <ilorn.mc@gmail.com>
2015-01-29 11:44:45 +0800

    Update example

--- example/upload.php
+++ example/upload.php
@@ -6,10 +6,8 @@ Autoload::register();
 
 $accessKey = 'accessKey';
 $secretKey = 'secretKey';
-
 $qiniu = new \Qiniu\Qiniu($accessKey, $secretKey);
-$bucket = $qiniu->getBucket('sketch');
 
-// 上传文件函数
+$bucket = $qiniu->getBucket('sketch');
 $response = $bucket->put($_FILES['file']['tmp_name'], "test.png");
 echo $response->getContent();
